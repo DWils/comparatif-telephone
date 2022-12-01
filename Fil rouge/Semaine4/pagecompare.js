955b79858deae8ca34f8b720158ce8e9279f9c7d
Nancy Moussima <ghislainemouss20@gmail.com>
2022-12-01 20:24:58 +0100

    pagedecomparaison

---
+++ Fil rouge/Semaine4/pagecompare.js
@@ -0,0 +1,111 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png",
+    "rakuten" : "",
+    "amazon": "",
+    "cdiscount": ""
+}, {
+    "id": 1,
+    "title": "Samsung Galaxy S20 FE 5G",
+    "price-rakuten": "289€",
+    "price-amazon": "415€",
+    "price-cdiscount": "339€",
+    "description": "Le Samsung Galaxy S20 Fan Edition apparait comme une version plus pragmatique du Galaxy S20. Une fiche technique construite sur l'avis des fans et qui comporte notamment un SoC Qualcomm, réclamé par beaucoup en Europe.",
+    "brand": "Samsung",
+    "image": "Images/Samsungs20.jpg",
+    "rakuten" : "https://fr.shopping.rakuten.com/mfp/6575826/samsung-galaxy-s20-fan-edition-5g?pid=5504650608&t=2703347&eff_cpt=22547069&",
+    "amazon": "https://www.amazon.fr/dp/B08HN8F1MC?ascsubtag=AM_LN_AF____B________&tag=lmdn-api-21&linkCode=ogi&th=1&psc=1&smid=A20IHUMHDHIEXS",
+    "cdiscount": "https://www.cdiscount.com/telephonie/telephone-mobile/samsung-galaxy-s20-fe-5g-rouge-reconditionne-e/f-1440402-rcdbycds451ec.html?idOffre=2299193407&awc=6948_1669917373_612eb472aac89faf8664a6363de36855&cid=affil&cm_mmc=zanoxpb-_-268983"
+}, {
+    "id": 2,
+    "title": "Xiaomi 12T Pro",
+    "price-rakuten": "616€",
+    "price-amazon": "688€",
+    "price-cdiscount": "716€",
+    "description": "Après un modèle 12 Pro en demi-teinte, Xiaomi complète son catalogue de smartphones de fin d'année avec sa gamme T. Le 12T Pro en est la vitrine et se présente avec une arme de taille, son capteur photo principal de 200 mégapixels.",
+    "brand": "Xiaomi",
+    "image": "Images/xiaomi photo.png",
+    "rakuten" : "https://fr.shopping.rakuten.com/mfp/8651077/xiaomi-12t-pro-5g?pid=9334478803&t=2703347&eff_cpt=22547069&",
+    "amazon": "https://www.amazon.fr/dp/B0BG5V8ZLS?ascsubtag=AM_LN_AF____B________&tag=lmdn-api-21&linkCode=osi&th=1&psc=1&smid=AI7RP6ZKPO6T",
+    "cdiscount": "https://www.cdiscount.com/telephonie/telephone-mobile/xiaomi-12t-pro-8-go-256-go-bleu-clair/f-1440402-xia6934177797569.html?idOffre=2385863106&awc=6948_1669917855_c808f0fdaca56be0209c55eb8c6122dc&cid=affil&cm_mmc=zanoxpb-_-268983"
+}, {
+    "id": 3,
+    "title": "Iphone 13 Pro",
+    "price-rakuten": "1 122€",
+    "price-amazon": "rupture stock",
+    "price-cdiscount": "rupture stock",
+    "description": "Petit frère de la version Pro Max, l'iPhone 13 Pro ne diffère pas tellement du gros modèle. Seule son autonomie est revue à la baisse, mais le reste de ses capacités est du même niveau, dans un format plus compact.",
+    "brand": "Apple",
+    "image": "Images/photoIphone.jpg",
+    "rakuten" : "https://fr.shopping.rakuten.com/mfp/7270536/apple-iphone-13-pro?pid=7197507121&t=2703347&eff_cpt=22547069&",
+    "amazon": "https://www.amazon.fr/Apple-iPhone-13-Pro-128-Go-Comprend-EarPods/dp/B09G9KKDWG",
+    "cdiscount": "https://www.cdiscount.com/telephonie/telephone-mobile/apple-iphone-13-pro-128-go-sierra-blue-2021-re/f-1440402-rcdapp0050236tbe.html?idOffre=2123362890#mpos=0|mp"
+}, {
+    "id": 4,
+    "title": "Redmi note 11",
+    "price-rakuten": "330€",
+    "price-amazon": "336€",
+    "price-cdiscount": "Rupture stock",
+    "description": "Le Redmi Note 11 Pro+ 5G couronne la gamme Redmi Note 11 de Xiaomi, promettant les performances les plus élevées et la charge la plus rapide. Un dernier point indéniable, mais qui ne fait pas de lui le meilleur rapport qualité-prix de sa famille.",
+    "brand": "Xiaomi",
+    "image": "Images/Redmi note 11 pro 5G.png",
+    "rakuten" : "https://fr.shopping.rakuten.com/mfp/7678589/xiaomi-redmi-note-11-pro-plus-5g?pid=8259106885&t=2703347&eff_cpt=22547069&",
+    "amazon": "https://www.amazon.fr/dp/B09VNZ8RVG?ascsubtag=AM_LN_AF____B________&tag=lmdn-api-21&linkCode=ogi&th=1&psc=1&smid=A27ZBC04L1HL54",
+    "cdiscount": "https://www.cdiscount.com/telephonie/telephone-mobile/xiaomi-redmi-note-11-128go-4g-bleu/f-1440438-xiaoredmi11128b.html?idOffre=2366542894#mpos=0|mp"
+}, {
+    "id": 5,
+    "title": "Iphone 12 mini",
+    "price-rakuten": "528€",
+    "price-amazon": "859€",
+    "price-cdiscount": "Rupture stock",
+    "description": "D'abord attendu, puis réclamé et fantasmé, l'iPhone 12 Mini est maintenant réalité, avec sa petite taille qui attire les regards. Nous nous sommes donc empressés de voir ce qu'il avait dans le ventre. Il n'est pas grand, mais il est vaillant.",
+    "brand": "Apple",
+    "image": "Images/iphone12mini.jpg",
+    "rakuten" : "https://fr.shopping.rakuten.com/mfp/6645334/apple-iphone-12-mini-avec-earpods?pid=5601189520&t=2703347&eff_cpt=22547069&",
+    "amazon": "https://www.amazon.fr/dp/B08L5RY2CG?ascsubtag=AM_LN_AF____B________&tag=lmdn-api-21&linkCode=osi&th=1&psc=1&smid=A1X6FK5RDHNB96",
+    "cdiscount": "https://www.cdiscount.com/telephonie/telephone-mobile/apple-iphone-12-mini-64go-blanc-reconditionne/f-1440402-rcdbycds069cn.html?idOffre=2306060358#mpos=0|mp"
+}]
+for (var i=1;i<product.length; i++) {
+    document.getElementById("select1").innerHTML +=`<option value="${i}">${product[i].title}</option>`;
+    document.getElementById("select2").innerHTML +=`<option value="${i}">${product[i].title}</option>`;
+}
+
+function item1(a) {
+    var select2 = document.getElementById("select2").value;
+    if(a != select2) {
+        document.getElementById("img1").src=product[a].image
+        document.getElementById("rakuten1").href = product[a].rakuten;
+        document.getElementById("amazon1").href = product[a].amazon;
+        document.getElementById("cdiscount1").href = product[a].cdiscount;
+        document.getElementById("price-rakuten1").textContent = product[a]["price-rakuten"];
+        document.getElementById("price-amazon1").textContent = product[a]["price-amazon"];
+        document.getElementById("price-cdiscount1").textContent = product[a]["price-cdiscount"];
+        document.getElementById("description1").innerHTML=product[a].description
+        document.getElementById("brand1").innerHTML=product[a].brand
+    }
+    else {
+        document.getElementById("img1").src=product[0].image;
+    }
+}
+
+function item2(a) {
+    var select1 = document.getElementById("select1").value;
+    if(a != select1) {
+        document.getElementById("img2").src=product[a].image
+        document.getElementById("rakuten2").href = product[a].rakuten;
+        document.getElementById("amazon2").href = product[a].amazon;
+        document.getElementById("cdiscount2").href = product[a].cdiscount;
+        document.getElementById("price-rakuten2").textContent = product[a]["price-rakuten"];
+        document.getElementById("price-amazon2").textContent = product[a]["price-amazon"];
+        document.getElementById("price-cdiscount2").textContent = product[a]["price-cdiscount"];
+        document.getElementById("description2").innerHTML=product[a].description
+        document.getElementById("brand2").innerHTML=product[a].brand
+    }
+    else {
+        document.getElementById("img2").src=product[0].image;
+    }
+}
